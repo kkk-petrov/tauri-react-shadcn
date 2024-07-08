2ca47a318822d216e0ec29f79cade4953bd97893
Kirill Petrov <96110988+kkk-petrov@users.noreply.github.com>
2024-07-08 11:47:18 +0300

    Delete src/app/router.tsx

--- src/app/router.tsx
+++
@@ -1,9 +0,0 @@
-import { Main } from "@/pages/main"
-import { Loader } from "lucide-react"
-import { Suspense } from "react"
-
-export const Router = () => {
-  return <Suspense fallback={<Loader className="animate-spin bg-blue-500" />}>
-    <Main />
-  </Suspense>
-}
